cec36dd7aa7d5b1b425f003ff1e8cf1747b90c76
Larissa <larissa.mikkers@hotmail.com>
2024-03-20 16:28:37 +0100

    Copy van eerste project Amsterdam West buurt initiatief

--- server.js
+++ server.js
@@ -1,1 +1,63 @@
 console.log('Hier komt je server')
+
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'))
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+
+// TODO: routes voor de amsterdam buurt initiatieven
+
+// Homepage 
+app.get('/', function(request, response) {
+	fetchJson('https://fdnd-agency.directus.app/items/dh_services').then((servicesDataUitDeAPI) => {
+		response.render('homepage', {services: servicesDataUitDeAPI.data})
+	});
+})
+// Contact pagina 
+app.get('/contact', function(request, response) {
+	response.render('contact')
+})
+
+// Vraag en aanbod pagina 
+app.get('/vraag-aanbod', function(request, response) {
+
+	fetchJson('https://fdnd-agency.directus.app/items/dh_services').then((servicesDataUitDeAPI) => {
+		response.render('vraag-aanbod', {services: servicesDataUitDeAPI.data})
+	});
+	
+})
+
+// Vraag en aanbod detail(id) pagina 
+app.get('/vraag-aanbod/:serviceId', function(request, response) {
+	fetchJson('https://fdnd-agency.directus.app/items/dh_services?filter={"id":' + request.params.serviceId + '}').then((serviceDetail) => {
+		response.render('service', {service: serviceDetail.data[0]})
+	})
+})
+
+
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function() {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
